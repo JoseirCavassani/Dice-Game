3f845bee85051e9bd4fb316f16cce9539fc0ad8b
JoseirCavassani <joseir.cavassani@hotmail.com>
2023-02-13 07:58:35 +0000

    First commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function loadPage() {
+  document.getElementById("img1").setAttribute("src", "images/dice1.png");
+  document.getElementById("img2").setAttribute("src", "images/dice2.png");
+}
+
+function rollDice() {
+  document.getElementById("img1").setAttribute("src", "images/dice3.png");
+  document.getElementById("img2").setAttribute("src", "images/dice4.png");
+  console.log("test");
+}
